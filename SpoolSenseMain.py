b71166f893b4aa375fa389b74fdf341cb876e8f6
unk286 <177930405+unk286@users.noreply.github.com>
2026-01-24 20:28:07 +0000

    Update SpoolSenseMain.py

--- SpoolSenseMain.py
+++ SpoolSenseMain.py
@@ -1,6 +1,14 @@
 #MAIN SCRIPT
 #Created by thomasse10 on GitHub
 
+#Unfinished main script
+
+#Debug
+debug = True
+def debugprint(msg):
+    if debug:
+        print(f"[DEBUG] {msg}")
+
 #imports
 import json
 
@@ -11,9 +19,31 @@ spools = []
 def create_spool():
     print("--Create Spool--")
     sbrand = input("Enter brand of Filament: ").strip()
+    while not sbrand:
+        sbrand = input("Cannot be empty.\nEnter brand of Filament: ").strip()
+    if sbrand:
+        sbrand = sbrand.lower()
+        debugprint(sbrand)
     stype = input("Enter type of Filament: ").strip()
+    while not stype:
+        stype = input("Cannot be empty.\nEnter type of Filament: ").strip()
+    if stype:
+        stype = stype.lower()
+        debugprint(stype)
     scolour = input("Enter colour of Filament: ").strip()
-    sweight = input("Enter weight of spool: ").strip()
+    while not scolour:
+        scolour = input("Cannot be empty.\nEnter colour of Filament: ").strip()
+    if scolour:
+        scolour = scolour.lower()
+        debugprint(scolour)
+#    sweight = input("Enter weight of spool: ").strip()
+#    while not sweight:
+#        sweight = input("Enter weight of Filament: ").strip()
+#    if sweight:
+#        sweight = sweight.lower()
+    newspool = sbrand, stype, scolour
+    debugprint(newspool)
+    return newspool
 
 
 def main_menu():
